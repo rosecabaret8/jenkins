af6ee9b7d5eaed6eb6311dd7d1a4ec76736c34e4
rosecabaret8 <83454378+rosecabaret8@users.noreply.github.com>
2021-05-19 15:15:55 +0200

    Update deploy_remote.sh

--- deploy_remote.sh
+++ deploy_remote.sh
@@ -6,6 +6,6 @@ then
   sudo amazon-linux-extras install -y java-openjdk11 
 fi
 
-ssh -o StrictHostKeyChecking=no -i /secrets/kp-Rose.pem ec2-user@ec2-52-26-143-177.us-west-2.compute.amazonaws.com 'sudo mkdir -p /app'
+ssh -o StrictHostKeyChecking=no -i /secrets/kp-Rose.pem ec2-user@ec2-52-26-143-177.us-west-2.compute.amazonaws.com 'sudo mkdir -p /app && chmod -R 666 /app'
 scp -o StrictHostKeyChecking=no -i /secrets/kp-Rose.pem helloWorld.class ec2-user@ec2-54-212-187-128.us-west-2.compute.amazonaws.com:/app
 ssh -o StrictHostKeyChecking=no -i /secrets/kp-Rose.pem ec2-user@ec2-52-26-143-177.us-west-2.compute.amazonaws.com 'sudo cd /app && sudo java helloWorld'

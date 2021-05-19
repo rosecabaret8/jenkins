de918b822d93a294ff1eb878195ea451a75c94c8
rosecabaret8 <83454378+rosecabaret8@users.noreply.github.com>
2021-05-19 15:41:17 +0200

    Update deploy_remote.sh

--- deploy_remote.sh
+++ deploy_remote.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 #deploy_remote.sh
 
+#Je mets la chaine de connexion de ma machine distance dans une variable
 MACHINE_DESTINATION=ec2-user@ec2-52-26-143-177.us-west-2.compute.amazonaws.com
+PATH_SECRET_KEY=/secrets/kp-Rose.pem
 
-ssh -o StrictHostKeyChecking=no -i /secrets/kp-Rose.pem $MACHINE_DESTINATION 'sudo amazon-linux-extras install -y java-openjdk11'
+#On installe Java sur la machine distante 
+#ne pas oublier de mettre -o StrictHostKeyChecking=no pour ssh afin d'être non-interactif sur l'acception de la vérification de host
+#ne pas oublier de mettre -y pour yum ou amazon-linux-extras ou apt afin d'être non-interactif sur l'acception de l'installation
+ssh -o StrictHostKeyChecking=no -i $PATH_SECRET_KEY $MACHINE_DESTINATION 'sudo amazon-linux-extras install -y java-openjdk11'
 
-ssh -o StrictHostKeyChecking=no -i /secrets/kp-Rose.pem $MACHINE_DESTINATION 'sudo mkdir -p /app && sudo chmod -R 777 /app && sudo chown ec2-user /app'
-scp -o StrictHostKeyChecking=no -i /secrets/kp-Rose.pem helloWorld.class $MACHINE_DESTINATION:/home/ec2-user/helloWorld.class
-ssh -o StrictHostKeyChecking=no -i /secrets/kp-Rose.pem $MACHINE_DESTINATION 'sudo cp /home/ec2-user/helloWorld.class /app'
-ssh -o StrictHostKeyChecking=no -i /secrets/kp-Rose.pem $MACHINE_DESTINATION 'sudo cd /app && sudo java helloWorld'
+#On crée le répertoire /app sur la machine distante
+ssh -o StrictHostKeyChecking=no -i $PATH_SECRET_KEY $MACHINE_DESTINATION 'sudo mkdir -p /app && sudo chmod -R 777 /app && sudo chown ec2-user /app'
+
+#On copie le fichier .class dans le dossier homedir de l'utilisateur (car on n'arrive pas à l'envoyer directement sur /app)
+#possible de faire dans un dossier temporaire 
+scp -o StrictHostKeyChecking=no -i $PATH_SECRET_KEY helloWorld.class $MACHINE_DESTINATION:/home/ec2-user/helloWorld.class
+
+#On copie le fichier .class dans le dossier /app localement 
+ssh -o StrictHostKeyChecking=no -i $PATH_SECRET_KEY $MACHINE_DESTINATION 'sudo cp /home/ec2-user/helloWorld.class /app'
+
+#On execute le fichier helloWorld.class
+ssh -o StrictHostKeyChecking=no -i $PATH_SECRET_KEY $MACHINE_DESTINATION 'sudo cd /app && sudo java helloWorld'

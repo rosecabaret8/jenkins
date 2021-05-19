b4d410d148c5174cc623785eb010f615e25769fb
rosecabaret8 <83454378+rosecabaret8@users.noreply.github.com>
2021-05-19 14:48:09 +0200

    Update deploy_remote.sh

--- deploy_remote.sh
+++ deploy_remote.sh
@@ -3,8 +3,6 @@
 java -version
 if [[$? !=0 ]] then sudo amazon-linux-extras
 install -y java-openjdk11 fi
-scp ssh -i "kp-Rose.pem" ec2-user@ec2-54-212-187-128.us-west-2.compute.amazonaws.com/app
-helloWorld.class
+scp ssh -i "kp-Rose.pem" ec2-user@ec2-54-212-187-128.us-west-2.compute.amazonaws.com/app helloWorld.class
 scp -i /secrets/kp-Rose.pem helloWorld.class ec2-user@IP:/app
-ssh kp-Rose.pem" ec2-user@ec2-52-26-143-177.us-west-2.compute.amazonaws.com -c'cd/app && java
-helloWorld
+ssh kp-Rose.pem" ec2-user@ec2-52-26-143-177.us-west-2.compute.amazonaws.com -c'cd/app && java helloWorld

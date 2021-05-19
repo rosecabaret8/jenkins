c34df1f8ac46cf318f2b88d30f87377c224a3438
rosecabaret8 <83454378+rosecabaret8@users.noreply.github.com>
2021-05-19 15:05:53 +0200

    Update deploy_remote.sh

--- deploy_remote.sh
+++ deploy_remote.sh
@@ -7,5 +7,5 @@ then
 fi
 
 ssh -i /secrets/kp-Rose.pem ec2-user@ec2-52-26-143-177.us-west-2.compute.amazonaws.com 'sudo mkdir -p /app'
-scp -i /secrets/kp-Rose.pem helloWorld.class ec2-user@ec2-54-212-187-128.us-west-2.compute.amazonaws.com/app/
+scp -i /secrets/kp-Rose.pem helloWorld.class ec2-user@ec2-54-212-187-128.us-west-2.compute.amazonaws.com:/app
 ssh -i /secrets/kp-Rose.pem ec2-user@ec2-52-26-143-177.us-west-2.compute.amazonaws.com 'sudo cd /app && sudo java helloWorld'
